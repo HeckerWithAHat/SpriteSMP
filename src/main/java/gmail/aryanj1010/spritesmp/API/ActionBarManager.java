750b4d675cb161323dd9086d613ceaccc752d0ad
HeckerWithAHat <aryanj1010@gmail.com>
2023-12-13 10:28:18 -0800

    Fixed issue with Action Bar where it would not activate until all abilities had been used at least once

--- src/main/java/gmail/aryanj1010/spritesmp/API/ActionBarManager.java
+++ src/main/java/gmail/aryanj1010/spritesmp/API/ActionBarManager.java
@@ -37,6 +37,10 @@ public class ActionBarManager {
 
             switch (ps.getSprite(p).toString()) {
                 case "FireSprite": {
+                    if (fireRight.get(p.getUniqueId()) == null) fireRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (fireShiftRight.get(p.getUniqueId()) == null) fireShiftRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (firePickaxeRight.get(p.getUniqueId()) == null) firePickaxeRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (fireShovelRight.get(p.getUniqueId()) == null) fireShovelRight.put(p.getUniqueId(), System.currentTimeMillis());
                     swordRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - fireRight.get(p.getUniqueId()))) / 1000))));
                     swordShiftRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - fireShiftRight.get(p.getUniqueId()))) / 1000))));
                     pickaxeRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - firePickaxeRight.get(p.getUniqueId()))) / 1000))));
@@ -44,6 +48,10 @@ public class ActionBarManager {
                 }
                 break;
                 case "WaterSprite": {
+                    if (waterRight.get(p.getUniqueId()) == null) waterRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (waterShiftRight.get(p.getUniqueId()) == null) waterShiftRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (waterPickaxeRight.get(p.getUniqueId()) == null) waterPickaxeRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (waterShovelRight.get(p.getUniqueId()) == null) waterShovelRight.put(p.getUniqueId(), System.currentTimeMillis());
                     swordRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - waterRight.get(p.getUniqueId()))) / 1000))));
                     swordShiftRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - waterShiftRight.get(p.getUniqueId()))) / 1000))));
                     pickaxeRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - waterPickaxeRight.get(p.getUniqueId()))) / 1000))));
@@ -51,6 +59,10 @@ public class ActionBarManager {
                 }
                 break;
                 case "AirSprite": {
+                    if (airRight.get(p.getUniqueId()) == null) airRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (airShiftRight.get(p.getUniqueId()) == null) airShiftRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (airPickaxeRight.get(p.getUniqueId()) == null) airPickaxeRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (airShovelRight.get(p.getUniqueId()) == null) airShovelRight.put(p.getUniqueId(), System.currentTimeMillis());
                     swordRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - airRight.get(p.getUniqueId()))) / 1000))));
                     swordShiftRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - airShiftRight.get(p.getUniqueId()))) / 1000))));
                     pickaxeRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - airPickaxeRight.get(p.getUniqueId()))) / 1000))));
@@ -58,6 +70,10 @@ public class ActionBarManager {
                 }
                 break;
                 case "EarthSprite": {
+                    if (earthRight.get(p.getUniqueId()) == null) earthRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (earthShiftRight.get(p.getUniqueId()) == null) earthShiftRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (earthPickaxeRight.get(p.getUniqueId()) == null) earthPickaxeRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (earthShovelRight.get(p.getUniqueId()) == null) earthShovelRight.put(p.getUniqueId(), System.currentTimeMillis());
                     swordRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - earthRight.get(p.getUniqueId()))) / 1000))));
                     swordShiftRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - earthShiftRight.get(p.getUniqueId()))) / 1000))));
                     pickaxeRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - earthPickaxeRight.get(p.getUniqueId()))) / 1000))));
@@ -65,6 +81,10 @@ public class ActionBarManager {
                 }
                 break;
                 case "ShadowSprite": {
+                    if (shadowRight.get(p.getUniqueId()) == null) shadowRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (shadowShiftRight.get(p.getUniqueId()) == null) shadowShiftRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (shadowPickaxeRight.get(p.getUniqueId()) == null) shadowPickaxeRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (shadowShovelRight.get(p.getUniqueId()) == null) shadowShovelRight.put(p.getUniqueId(), System.currentTimeMillis());
                     swordRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - shadowRight.get(p.getUniqueId()))) / 1000))));
                     swordShiftRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - shadowShiftRight.get(p.getUniqueId()))) / 1000))));
                     pickaxeRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - shadowPickaxeRight.get(p.getUniqueId()))) / 1000))));
@@ -72,6 +92,10 @@ public class ActionBarManager {
                 }
                 break;
                 case "GlowSprite": {
+                    if (glowRight.get(p.getUniqueId()) == null) glowRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (glowShiftRight.get(p.getUniqueId()) == null) glowShiftRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (glowPickaxeRight.get(p.getUniqueId()) == null) glowPickaxeRight.put(p.getUniqueId(), System.currentTimeMillis());
+                    if (glowShovelRight.get(p.getUniqueId()) == null) glowShovelRight.put(p.getUniqueId(), System.currentTimeMillis());
                     swordRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - glowRight.get(p.getUniqueId()))) / 1000))));
                     swordShiftRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - glowShiftRight.get(p.getUniqueId()))) / 1000))));
                     pickaxeRight = String.valueOf(Math.max(0, Math.round((float) (((60 * 1000L) - (System.currentTimeMillis() - glowPickaxeRight.get(p.getUniqueId()))) / 1000))));
